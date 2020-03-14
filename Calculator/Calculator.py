e454377f01c7e69b1a9113313936e0ce8222b440
kn285 <55518570+kn285@users.noreply.github.com>
2020-03-14 13:19:50 -0400

    Task: fixed Calculator

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -1,6 +1,10 @@
 from MathOperations.addition import Addition
 from MathOperations.subtraction import Subtraction
-
+from MathOperations.multiplication import Multiplication
+from MathOperations.division import Division
+from MathOperations.square import Square
+from MathOperations.root import root
+from MathOperations.logarithm import logarithm
 
 class Calculator:
     Result = 0
@@ -14,4 +18,24 @@ class Calculator:
 
     def Difference(self, a, b):
         self.Result = Subtraction.difference(a, b)
+        return self.Result
+
+    def Multiplication(self, a, b):
+        self.Result = Multiplication.multiply(a,b)
+        return self.Result
+
+    def Division(self, a, b):
+        self.Result = Division.divide(a,b)
+        return self.Result
+
+    def Square(self, a, b):
+        self.Result = Square.square(a,b)
+        return self.Result
+
+    def nthroot(self, a, b):
+        self.Result = root.root(a, b)
+        return self.Result
+
+    def log(self, a, b):
+        self.Result = logarithm(a,b)
         return self.Result
